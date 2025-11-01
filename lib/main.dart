c285a60b14b22690b2eee5f7245a326e30712f46
mhz1123 <60521526+mhz1123@users.noreply.github.com>
2025-11-01 09:40:30 +0530

    Integrate Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,14 @@ import 'package:provider/provider.dart';
 import './providers/app_provider.dart';
 import './widgets/app_shell.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
